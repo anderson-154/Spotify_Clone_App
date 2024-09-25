526f50d271423b5aefd80532f8ccb503b9c442fa
Anderson <andyguaca@gmail.com>
2024-09-25 15:32:07 -0500

    complete the appTheme

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -20,4 +20,22 @@ class AppTheme {
       )
     )
   );
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    )
+  );
 }
